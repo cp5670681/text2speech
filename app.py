f45d29f8c4fee363e904e695470b3f05e8375a91
cp5670681 <986806236@qq.com>
2020-11-21 16:40:10 +0800

    1

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
+import json
+import io
+
+import requests
 from flask import Flask, request
 from wechatpy import parse_message
-from wechatpy.replies import TextReply
+from wechatpy.replies import TextReply, VoiceReply
+from flask_caching import Cache
+
+cache = Cache()
 
 app = Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 1 * 60 * 60  # 默认过期时间 5分钟
+cache.init_app(app)
+
+
+with open('config.json', encoding='utf-8') as f:
+    config = json.load(f)
+
+@cache.cached(key_prefix='get_access_token')
+def get_access_token():
+
+    params = {
+        'grant_type': 'client_credential',
+        'appid': config['appid'],
+        'secret': config['app_secret']
+    }
+    r = requests.get('https://api.weixin.qq.com/cgi-bin/token', params=params)
+    access_token = r.json().get('access_token')
+    return access_token
+
+def upload(text):
+    access_token = get_access_token()
+    r = requests.get('http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=2&text={}'.format(text))
+    files = {
+        'media': ('voice.mp3', io.BytesIO(r.content), 'application/audio/x-mpeg')
+    }
+    r2 = requests.post('https://api.weixin.qq.com/cgi-bin/media/upload?access_token={}&type=voice'.format(access_token), files=files)
+    media_id = r2.json()['media_id']
+    return media_id
+
+upload('你好')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,7 +51,8 @@ def index():
     print(xml)
     msg = parse_message(xml)
     if msg.type == 'text':
-        reply = TextReply(content=msg.content, message=msg)
+        media_id = upload(msg.content)
+        reply = VoiceReply(media_id=media_id, message=msg)
         return reply.render()
     return ''
 
