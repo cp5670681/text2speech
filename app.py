1698e83a409ed2ab739d16c97372eb3474ece935
cp5670681 <986806236@qq.com>
2020-11-21 15:10:46 +0800

    1

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'GET':
+        return request.args.get('echostr')
     print(request.get_data().decode())
     return 'Hello World!'
 
